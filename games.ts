35c8f62ac5f978471fc1379aa9f37396d9b3d11c
Elton6 <107364141+Elton6@users.noreply.github.com>
2022-06-17 14:16:55 -0700

    Initial files for MakeCode project

---
+++ games.ts
@@ -0,0 +1,15 @@
+//% color="#AA278D" weight=100
+namespace hello {
+    //% block
+    export function helloWorld() {
+
+    }
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block
+    export function camlCaseTwo() {
+
+    }
+}
