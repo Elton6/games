056eb81ebf62fbc2def36d43551acaef17a1748e
Elton6 <107364141+Elton6@users.noreply.github.com>
2022-06-17 14:38:35 -0700

    Update tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -1,7 +1,7 @@
 // Auto-generated code. Do not edit.
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
-    export const transparency16 = image.ofBuffer(hex``);
+    export const transparency17 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
